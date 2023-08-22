3a2c5a90b96d0aa022ac95d7969672ab646339f1
dialop <dianalop1998@gmail.com>
2023-08-22 17:35:58 -0400

    Implemented exncrypted cookies

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require("cookie-parser");
 const e = require("express");
+const bcrypt = require("bcryptjs");
+const cookieSession = require("cookie-session"); // import the cookie session
+
 
 app.set("view engine", "ejs");
 
@@ -62,14 +64,19 @@ const urlsForUser = function(id) {
 };
 
 
-
 //Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+
+app.use(cookieSession({
+  name: "session",
+  keys: ["secret_key_for_encryption"],
+  maxAge: 24 * 60 * 60 * 1000
+
+}));
+
 
 app.post("/urls", (req, res) => {
-  const user = users[req.cookies["user_id"]];
-  const longURL = req.body.longURL;
+  const user = users[req.session.user_id]; longURL = req.body.longURL;
 
   if (!user) {
     res.status(403).send("You must be logged into shorten URL.");  //repond with HTML message with code status restriction
@@ -85,7 +92,7 @@ app.post("/urls", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {  // code to implement a DELETE operation to remove existing shortened URLs
   const shortURL = req.params.id;
-  const user = users[req.cookies["user_id"]];
+  const user = users[req.session.user_id];
 
   if (!user) {
     res.status(403).send("You need to log in to delete URLs.");
@@ -99,7 +106,7 @@ app.post("/urls/:id/delete", (req, res) => {  // code to implement a DELETE oper
 
 app.post("/urls/:id", (req, res) => { // implementing the redirect link after using "edit" button on localhost:8080/urls/
   const shortURL = req.params.id;
-  const user = users[req.cookies["user_id"]];
+  const user = users[req.session.user_id];
 
   if (!user) {
     res.status(403).send("You need to log in to edit URLs.");
@@ -113,7 +120,7 @@ app.post("/urls/:id", (req, res) => { // implementing the redirect link after us
 app.post("/urls/:id/edit", (req, res) => {    //implementing the post edit
   const shortURL = req.params.id;
   const newLongURL = req.body.longURL;
-  const user = users[req.cookies["user_id"]];
+  const user = users[req.session.user_id];
 
   if (!user) {
     res.status(403).send("You need to log in to edit URLs.");
@@ -126,20 +133,20 @@ app.post("/urls/:id/edit", (req, res) => {    //implementing the post edit
 });
 
 app.post("/login", (req, res) => {
-  const { email, password } = req.body; // implementing cookie user_id
+  const { email, password } = req.body;
   const user = Object.values(users).find(u => u.email && u.password === password);
 
-  if (user) {
-    res.cookie("user_id", user.id);
+  if (user && bcrypt.compareSync(password, user.password)) {  //successful login
+    req.session.user_id = user.id;
     res.redirect("/urls");
   } else {
-    res.status(403).send("Cannot Authorize Request Email or Password Invalid.");
+    res.status(403).send("Unable to Authorize! Email or Password Invalid.");   //failed login
   }
 });
 
-app.post("/logout", (req, res) => { // clear user_id cookie
-  res.clearCookie("user_id");
-  res.redirect("/urls");
+app.post("/logout", (req, res) => {
+  req.session = null;       //clears session data for client
+  res.redirect("/login");   // redirect to login page
 });
 
 app.post("/register", (req, res) => {
@@ -154,13 +161,15 @@ app.post("/register", (req, res) => {
   }
 
   const userid = generateRandomString(6);
+  const hashedPassword = bcrypt.hashSync(password, 10);   //Hash the password
+
   users[userid] = {
     id: userid,
     email: email,
-    password: password
+    password: hashedPassword       //Store Hash Password
   };
 
-  res.cookie("user_id", userid);
+  req.session.user_id = userid;
   res.redirect("/urls");
 });
 
@@ -185,7 +194,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const user = users[req.cookies["user_id"]];
+  const user = users[req.session.user_id];     //retrives user id data
   if (!user) {
     res.redirect("/login");
   } else {
@@ -199,7 +208,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const user = users[req.cookies["user_id"]]; //redirecting unautheenticated users
+  const user = users[req.session.user_id]; //redirecting unautheenticated users
   if (!user) {
     res.redirect("/login");
   } else {
@@ -213,12 +222,12 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:id", (req, res) => { // display the username, the short URL ID, and the associated long URL
   const shortURL = req.params.id;
-  const user = users[req.cookies["user_id"]];
+  const user = users[req.session.user_id];
 
   if (!user) {
     res.render("login");
   } else if (!urlDatabase[shortURL] || urlDatabase[shortURL].userID !== user.id) {
-    res.render("login");                                                                // *******
+    res.render("login");
   } else {
     const templateVars = {
       id: shortURL,
@@ -242,7 +251,7 @@ app.get("/u/:id", (req, res) => { //code to implement short URL
 });
 
 app.get("/urls", (req, res) => {
-  const user = users[req.cookies["user_id"]];  // rendering the "urls_index" template for the the username of the currently logged in user
+  const user = users[req.session.user_id];  // rendering the "urls_index" template for the the username of the currently logged in user
     
   if (!user) {
     res.render("login");      //display urls for logged in users only
@@ -257,15 +266,15 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/register", (req, res) => { //route for register endpoint to render register.ejs tempelate
-  res.render("register", { user: req.cookies.user_id });
+  res.render("register", { user: req.session.user_id });
 });
 
 app.get("/login", (req, res) => {
-  res.render("login", { user: req.cookies.user_id });  //route for /login enspoint to render login.ejs tempelate
+  res.render("login", { user: req.session.user_id });  //route for /login enspoint to render login.ejs tempelate
 });
 
 app.get("/login", (req, res) => {   //redirecting when user logged in
-  const user = users[req.cookies["user_id"]];
+  const user = users[req.session.user_id];
 
   if (user) {
     res.redirect("/urls");
@@ -277,7 +286,7 @@ app.get("/login", (req, res) => {   //redirecting when user logged in
 });
 
 app.get("/register", (req, res) => {   //rendering registration page
-  const user = users[req.cookies["user_id"]];
+  const user = users[req.session.user_id];
 
   if (user) {
     res.redirect("/urls");
